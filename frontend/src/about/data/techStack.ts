6ca343c3bdd0c08e88e6fb5d7b8113fcef9eaa99
tnoori98 <thomas.noori@gmail.com>
2025-06-25 15:08:47 +0300

    updated: techstack

--- frontend/src/about/data/techStack.ts
+++ frontend/src/about/data/techStack.ts
@@ -4,7 +4,7 @@ export const techStack: TechStack[] =
 [
     { label: "Languages", items: ["C#", "TypeScript", "JavaScript", "SQL"] },
     { label: "Frameworks", items: ["ASP.NET MVC", "Blazor", "React", "Angular", "NestJS", "Express.js", "Next.js"] },
-    { label: "Tools", items: ["Docker", "Git", "Grafana", "PostgreSQL"] },
+    { label: "Tools", items: ["Docker", "Git", "OpenAI API", "Grafana", "PostgreSQL"] },
     { label: "Environments", items: ["Self-hosted Arch Linux server"] },
     { label: "Concepts", items: ["DevOps (CI/CD, Monitoring)", "Azure DevOps"] }
 ]

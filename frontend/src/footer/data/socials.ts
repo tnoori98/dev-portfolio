d7718894a140056a3988415d8b78f16561eb79eb
tnoori98 <thomas.noori@gmail.com>
2025-07-13 14:54:50 +0300

    updated: url

--- frontend/src/footer/data/socials.ts
+++ frontend/src/footer/data/socials.ts
@@ -1,5 +1,5 @@
 import type { SocialLink } from "../interfaces/SocialLink";
 
 export const socials: SocialLink[] = [{href:"https://github.com/tnoori98", name:"GitHub", icon:"/assets/github-mark-white.webp"},
-    {href:"linkedin.com/in/thomas-noori-721785373", name:"LinkedIn", icon:"/assets/InBug-White.webp"}
+    {href:"https://linkedin.com/in/thomas-noori-721785373", name:"LinkedIn", icon:"/assets/InBug-White.webp"}
 ]
